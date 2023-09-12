538e83528c9d7ec60099b7340e4833c98a96851b
Andrei Golubev <golubevcg@gmail.com>
2023-09-12 01:14:42 +0100

    small fix

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import sys
 from ui.hip_file_diff_window import HipFileDiffWindow
 from hutil.Qt.QtWidgets import QApplication
 
+
 def main(): 
     app = QApplication(sys.argv)
     window = HipFileDiffWindow()
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
